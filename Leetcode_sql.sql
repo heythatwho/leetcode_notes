fc84e2ea5d03083c9b56149428a1fe94b765b5bb
heythatwho <67612640+heythatwho@users.noreply.github.com>
2022-07-11 17:08:28 -0700

    Update Leetcode_sql.sql

--- Leetcode_sql.sql
+++ Leetcode_sql.sql
@@ -1221,3 +1221,29 @@ round( (select
 from Delivery a
 inner join Delivery b
 on a.delivery_id = b.delivery_id 
+
+
+
+
+
+#1294. Weather Type in Each Country
+Write an SQL query to find the type of weather in each country for November 2019.
+
+The type of weather is:
+
+Cold if the average weather_state is less than or equal 15,
+Hot if the average weather_state is greater than or equal to 25, and
+Warm otherwise.
+# Write your MySQL query statement below
+select
+country_name ,
+case when average_weather <=15 then 'Cold'
+    when average_weather>=25 then 'Hot'
+    else 'Warm' end as weather_type 
+from Countries c
+inner join (select country_id, avg(Weather_state) average_weather 
+           from weather 
+           where date_format(day, '%Y-%m')= '2019-11'  #yyyy-mm expression
+           group by country_id
+          ) w
+on c.country_id=w.country_id
